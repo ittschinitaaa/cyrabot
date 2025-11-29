e4d82c402fb8f8169a2308043afc29b1ec7ea062
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-11-29 02:59:32 -0300

    Update detectar-admin.js

--- plugins/detectar-admin.js
+++ plugins/detectar-admin.js
@@ -3,30 +3,25 @@ let handler = async (m, { conn }) => {}
 handler.all = async function (m) {
     if (!m.isGroup) return;
 
-    // texto leído del mensaje (texto o caption)
     let texto = (m.text || m.caption || "").toLowerCase();
 
-    // Palabras clave a detectar
-    const palabras = ["admin", "owner", "moderador", "staff"];
+    const palabras = ['admin', 'ADMIN', '"admin"', 'admi','ADMI','Admins','ADMINS',];
 
-    // Si el mensaje contiene alguna palabra
     if (palabras.some(w => texto.includes(w))) {
 
-        // Reacción del bot
         try {
             await m.react("🇨🇳");
         } catch (e) {
             console.log("Error en m.react", e);
         }
 
-        // RESPONDER citando al mensaje con #kick
         try {
             await this.sendMessage(
                 m.chat,
                 {
                     text: "#kick",
                 },
-                { quoted: m }  // ⇐ CITA el mensaje original
+                { quoted: m } 
             );
         } catch (e) {
             console.log("Error enviando #kick:", e);
@@ -36,6 +31,6 @@ handler.all = async function (m) {
 
 handler.help = ["autoadmin"];
 handler.tags = ["auto"];
-handler.command = []; // no es comando
+handler.command = []; 
 
 export default handler;
